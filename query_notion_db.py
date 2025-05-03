83c73bd63c400927bb67e0c7b3db1320b3e2c1dd
kyrieee <triple321jhango@gmail.com>
2025-05-04 06:37:47 +0900

    notion db get

---
+++ query_notion_db.py
@@ -0,0 +1,66 @@
+import os
+from pprint import pprint
+# from requests.exceptions import RequestException
+
+from notion_client import Client
+
+def lambda_handler(event, context):
+    # 例外が発生する可能性のある処理
+    try:
+        # 環境変数からトークンを取得
+        notion_token = os.getenv('NOTION_TOKEN')
+        if not notion_token:
+            raise ValueError("Notion token not found in environment variables")
+
+        # Notionクライアントの初期化
+        notion = Client(auth=notion_token)
+
+
+        # 対象のNotionDBかをクエリしてデータを取得
+        db = notion.databases.query(
+            database_id='NOTION_DB_ID'
+        )
+
+        return query_notion_db(db)
+
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": {
+                "message": "Error",
+                "error": str(e)
+            }
+        }
+        
+def query_notion_db(db):
+
+    pages_data = []
+
+    for page in db['results']:
+        # Title の取得（titleプロパティの最初の要素からtext.contentを取得）
+        title = page['properties']['Title']['title'][0]['text']['content']
+        
+        # URL の取得（urlプロパティを直接取得）
+        url = page['properties']['URL']['url']
+        
+        # Tag の取得（multi_selectからタグ名のリストを取得）
+        tags = [tag['name'] for tag in page['properties']['tag']['multi_select']]
+
+        # データを辞書形式で保存
+        pages_data.append({
+            "title": title,
+            "url": url,
+            "tags": tags
+        })
+    
+    print(f"取得レコード数: {len(pages_data)}")  # 取得レコード数がNotionDBと一致しているか確認
+
+    # 要素取得
+    return {
+        "statusCode": 200,
+        "body": {
+            "message": "Success",
+            "data": pages_data  # 全データをJSON形式で返す
+        }
+    }
